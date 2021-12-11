23006d87a3f8dfd4f3d4be760428ac68685d43d2
Edilainesds <edilaine45@yahoo.com.br>
2021-12-11 15:23:43 -0300

    atualizacao de rotas no app

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ const cors = require('cors')
  
 require('dotenv-safe').config() 
 const db = require('./database/mongoConfig')
+const routes = require("./routes/financasParaCriancasroutes")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-
+app.use("/financas", routes)
 db.connect()   
 
 module.exports = app
